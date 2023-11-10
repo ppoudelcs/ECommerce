4da5bb3284557cbf150978f605d6220472665ea7
ppoudelcs <ppoudel.cs@gmail.com>
2023-11-10 09:02:21 +0545

    created post login route

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -122,9 +122,17 @@ import Link from 'next/link';
 import { Alert } from 'antd';
 import { message } from 'antd';
 
+
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
 const SignupSchema = Yup.object().shape({
   
-  email: Yup.string().email('Invalid email').required('Required'),
+  // email: Yup.string().email('Invalid email').required('Required'),
+  phoneNumber: Yup.string()
+  .required("required")
+  .matches(phoneRegExp, 'Phone number is not valid')
+  .min(10, "too short")
+  .max(10, "too long"),
   password: Yup.string()
   .required('No password provided.') 
   .min(8, 'Password is too short - should be 8 chars minimum.')
@@ -133,7 +141,28 @@ const SignupSchema = Yup.object().shape({
 
 });
 
-const Home = () => (
+const Home = () => {
+
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const handleLogin = async(values)=>{
+    const res = await fetch('http://localhost:4000/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(values)
+    })
+
+    const data = await res.json()
+
+    messageApi.open({
+      type: res.status == 200 ? 'success' : 'error',
+      content: data.msg,
+    });
+
+    console.log(res)
+
+  }
+  return(
   <div>
     <Image
     src="/ktmEats.png"
@@ -141,26 +170,29 @@ const Home = () => (
     height={100}
     alt="Logo"
     />
+    {contextHolder}
 
     
     <h1>Sign In</h1>
     <Formik
       initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
+        phoneNumber: '',
+        password: '',
       }}
       validationSchema={SignupSchema}
       onSubmit={values => {
         // same shape as initial values
-        console.log(values);
+        handleLogin(values);
       }}
     >
       {({ errors, touched }) => (
         <Form>
-          <Field name="email" type="email" placeholder="Email" />
+          {/* <Field name="email" type="email" placeholder="Email" />
           {errors.email && touched.email ? <div>{errors.email}</div> : null}
           
+          <br/> */}
+          <Field name="phoneNumber" type="tel" placeholder="Enter phone number"/>
+          {errors.phoneNumber && touched.phoneNumber ? <div>{errors.phoneNumber}</div> : null}
           <br/>
           <Field name="password" type="password" placeholder="Password"/>
           {errors.password && touched.password ? (
@@ -175,5 +207,6 @@ const Home = () => (
     </Formik>
   </div>
 );
+}
 
 export default Home;
