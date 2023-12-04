f046ece06bef454711eb1ea6d05bcafde6b5f24b
ppoudelcs <ppoudel.cs@gmail.com>
2023-12-04 21:34:39 +0545

    created controllers on the server side

---
+++ server/src/controllers/user.js
@@ -0,0 +1,58 @@
+const User = require('../models/user')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
+const registerNewUser = async (req, res) => {
+
+ 
+    try {   
+     
+         const userExists = await User.findOne({phoneNumber: req.body.phoneNumber})
+         if (userExists) {
+           res.status(409).json({ msg: 'Phone number already taken' })
+           //??why conflict, user name can be duplicate but not phone number?
+     
+         } else {
+           const hashPassword = await bcrypt.hash(req.body.password, saltRounds)
+           req.body.password = hashPassword
+           const data = await User.create(req.body)
+     
+           if (data) res.json({ msg: 'user registered' })
+     
+     
+         }
+     
+       } catch (err) {
+         console.log(err)
+     
+       }
+     
+     }
+
+     const loginUser = async (req, res) => {
+        try {
+      
+          const userDetails = await User.findOne({phoneNumber: req.body.phoneNumber}).lean() //To exclude automatically created hidden data and features behind the scene
+          if (!userDetails) {
+            res.status(401).json({ msg: 'Invalid Credentials' })
+          } else {
+            const isMatched = await bcrypt.compare(req.body.password, userDetails.password)
+            if (isMatched) {
+              const token = jwt.sign({ phoneNumber: 9841000000 }, process.env.SECRET_KEY);
+              const {password, ...userInfo} = userDetails
+              
+              res.json({ msg: 'Login Success', token, userDetails:userInfo })
+            } else {
+              res.status(401).json({ msg: 'Incorrect password' })
+            }
+          }
+      
+      
+        } catch (err) {
+          console.log(err)
+        }
+      
+      }
+
+module.exports = {registerNewUser, loginUser}
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,62 +1,12 @@
 const express=require('express')
 const router=express.Router()
-const User = require('../models/user')
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-
-const jwt = require('jsonwebtoken');
 
 
-router.post('/register', async (req, res) => {
+const { registerNewUser, loginUser } = require('../controllers/user');
 
- 
- try {   
+
+router.post('/register', registerNewUser)
   
-      const userExists = await User.findOne({phoneNumber: req.body.phoneNumber})
-      if (userExists) {
-        res.status(409).json({ msg: 'Phone number already taken' })
-        //??why conflict, user name can be duplicate but not phone number?
-  
-      } else {
-        const hashPassword = await bcrypt.hash(req.body.password, saltRounds)
-        req.body.password = hashPassword
-        const data = await User.create(req.body)
-  
-        if (data) res.json({ msg: 'user registered' })
-  
-  
-      }
-  
-    } catch (err) {
-      console.log(err)
-  
-    }
-  
-  })
-  
-  router.post('/login', async (req, res) => {
-    try {
-  
-      const userDetails = await User.findOne({phoneNumber: req.body.phoneNumber}).lean() //To exclude automatically created hidden data and features behind the scene
-      if (!userDetails) {
-        res.status(401).json({ msg: 'Invalid Credentials' })
-      } else {
-        const isMatched = await bcrypt.compare(req.body.password, userDetails.password)
-        if (isMatched) {
-          const token = jwt.sign({ phoneNumber: 9841000000 }, process.env.SECRET_KEY);
-          const {password, ...userInfo} = userDetails
-          console.log(userInfo)
-          res.json({ msg: 'Login Success', token, userDetails:userInfo })
-        } else {
-          res.status(401).json({ msg: 'Incorrect password' })
-        }
-      }
-  
-  
-    } catch (err) {
-      console.log(err)
-    }
-  
-  })
+  router.post('/login', loginUser)
   
   module.exports=router;
